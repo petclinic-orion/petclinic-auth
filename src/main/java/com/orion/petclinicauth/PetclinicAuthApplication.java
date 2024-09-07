2cfa45960c2d26a381b19f4419ad88f527f83698
atokgoz <atokgoz@rbbn.com>
2024-09-07 20:32:07 +0300

    first commit

---
+++ src/main/java/com/orion/petclinicauth/PetclinicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.orion.petclinicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicAuthApplication.class, args);
+    }
+
+}
